d547897e18dc7478902237f9242801e292237a81
Davidhdm <david.hdm9@gmail.com>
2021-06-02 15:14:49 +0200

    create event model/factory/seeder

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Event;
+
+class EventFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'date_time' => $this->faker->dateTime(),
+            'title' => $this->faker->company(),
+            'image' => '',
+            'users_max' => $this->faker->randomNumber($nbDigits = 2),
+            'description' => $this->faker->company(),
+            'carousel' => false,
+        ];
+    }
+}
