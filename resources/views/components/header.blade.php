20d24e813a1fd9dcdd465a01b512005a7211b49b
Davidhdm <david.hdm9@gmail.com>
2021-06-03 13:45:45 +0200

    fixed css file routes

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,3 @@
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Sarina&display=swap');
-</style>
-
-<link rel ="stylesheet" href= '../../css/style.css'>
-
-
 <header class="header">
     <div class="containerKlik&Learn"></div>
     <h1 class="textHeader">Klik & Learn</h1>
