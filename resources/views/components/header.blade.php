3d44024c02cf39279c07eba253f470f35e51ff79
Berta LI <you@example.com>
2021-06-03 11:17:30 +0200

    act

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<header class="headerKlik&Learn">
+    <div class="containerKlik&Learn"></div>
+    <h1 class="textHeader">Klik & Learn</h1>
+    <a href="?action=index">
+        <!-- <button class="cto">
+            <img class="imgCto" src="./public/img/volver.svg">
+        </button> -->
+    </a>
+</header>
