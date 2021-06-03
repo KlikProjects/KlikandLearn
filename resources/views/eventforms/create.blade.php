492dd83bffa83477c143e1b6170cc23cf43d856a
Granaino BUAB <josemiguel@quesada.eu>
2021-06-03 14:25:11 +0200

    create view create and back for create new events

--- resources/views/eventforms/create.blade.php
+++ resources/views/eventforms/create.blade.php
@@ -1,12 +1,55 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+@extends('layouts.app')
 
-<div>
-    <form action="">
-        <input class="form-control mb-3 mtr-3" type="text" placeholder="Default input" aria-label="default input example">
-        <input class="form-control mb-3 mtr-3" type="text" placeholder="Default input" aria-label="default input example">
-        <input class="form-control mb-3 mtr-3" type="text" placeholder="Default input" aria-label="default input example">
-        <input class="form-control mb-3 mtr-3" type="text" placeholder="Default input" aria-label="default input example">
-        <input class="form-control mb-3 mtr-3" type="text" placeholder="Default input" aria-label="default input example">
-        <input class="form-control mb-3 mtr-3" type="text" placeholder="Default input" aria-label="default input example">
-    </form>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <p>Aqui se crea un nuevo evento</p>
+                    
+                    <form class="justify-content-center" action="{{route('home')}}" method="post">
+                    @csrf
+
+                        <div class="form-group">
+                            <span class="input-group-text">Date / Time</span>
+                            <input class="form-control" type="datetime" name="newdatetime" required placeholder="Date / Time yyyy-mm-dd hh:mm">
+                        </div>
+                        
+                        <div class="form-group">
+                            <span class="input-group-text">Title</span>
+                            <input class="form-control" type="text" name="newtitle" required placeholder="Title">
+                        </div>
+
+                        <div class="form-group">
+                            <span class="input-group-text">Description</span>
+                            <input class="form-control" type="text" name="newdescription" required placeholder="Description">
+                        </div>
+
+
+                        <div class="form-group">
+                            <span class="input-group-text">Image</span>
+                            <input class="form-control" type="text" name="newimage" required placeholder="url Image">
+                        </div>
+                        
+                        <div class="form-group">
+                            <span class="input-group-text">User Max</span>
+                            <input class="form-control" type="int" name="newusermax" required placeholder="Max Users">
+                        </div>
+                        
+                        <input class="btn-lg btn-outline-success go-add-task float" type="submit" value="Create">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection
