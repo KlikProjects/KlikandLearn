a1eced71388245d92480765bd124503fa1048143
Berta LI <you@example.com>
2021-06-07 08:31:01 +0200

    footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('footer')
+    <div class="containerFooter"></div>
+    <h1 class="textFooter">INICIO</h1>
+    <h2>LOGIN</h2>
+    <h3>LOGOUT</h2>
+    <h3>REG</h2>
+@endsection
