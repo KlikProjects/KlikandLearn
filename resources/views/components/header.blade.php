ed2d9945c490601b019569017bc86e8444345f5f
Gabriela <gabrielita21.gb@gmail.com>
2021-06-07 14:35:23 +0200

    insertando logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,6 +3,7 @@
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
                 <div class="containerKlik&Learn"></div>
+                <img src="./resources/img/logo.png">
                 <h1 class="textHeader">Klik & Learn</h1>{{-- {{ config('app.name', 'Laravel') }} --}}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
