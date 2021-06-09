e798099cecda3c56da64dc5456773038d8ecbdfe
Gabriela <gabrielita21.gb@gmail.com>
2021-06-09 15:35:56 +0200

    agregado imput image

--- resources/views/eventforms/create.blade.php
+++ resources/views/eventforms/create.blade.php
@@ -35,17 +35,19 @@
                             <span class="input-group-text" id="inputGroup-sizing-default">Description</span>
                             <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="description">
                         </div>
-                        
-                        <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-                            <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Image</span>
+                            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="url image">
                         </div>
 
-                        <input type="checkbox" class="btn-check form-switch sliderCB" id="btn-check-outlined" checked autocomplete="off">
-                        <label class="btn btn-outline-secondary" for="btn-check-outlined">carousel</label>
+                        <div class="form-check form-switch sliderCB">
+                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                            <label class="form-check-label" for="flexSwitchCheckChecked">Carousel</label>
+                        </div>
 
                         <div class="btnCreate">
-                            <input class="btn-lg btn-outline go-add-task float" type="submit" value="Create">
+                            <button type="submit" class="btn btn-outline-success" value="Create">Create</button>
                         </div>
                     </form>
                 </div>
