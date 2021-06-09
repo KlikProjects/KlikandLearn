76a4689faba75e51c9d9699e398dd72cf49c7c5a
Granaino BUAB <josemiguel@quesada.eu>
2021-06-09 10:47:44 +0200

    correction after merge of view list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,30 +52,6 @@
 
                 </article>
                 
-                <div class="titleAndDesc">
-                    <h3 class="eventTitle">{{$event->title}}</h3>
-                    <p class="eventDescription">{{$event->description}}</p>
-                </div>
-            </div>
-
-            <div class="imgBtnContainer">
-                <figure>
-                    <img class="imgEvents" src="{{$event->image}}" alt="">
-                </figure>
-                <button class="enrollBtn">Inscribirme</button>
-
-                <td>
-                    <form action="{{ route('events.destroy',$event->id) }}" method="POST">
-                        <a class="btn btn-sm btn-primary" href="{{ route('events.show',$event->id) }}"><i class="fa fa-fw fa-eye"></i>Show</a>
-                        <a class="btn btn-sm btn-success" href="{{ route('events.edit',$event->id) }}"><i class="fa fa-fw fa-edit"></i>Edit</a>
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>X</button>
-                    </form>
-                </td>
-
-            </div>
-        </article>
         <div class="line"></div>
         @endif
         @endforeach
