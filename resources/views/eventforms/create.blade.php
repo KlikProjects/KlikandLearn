e78e949bdbcb11d6d5b4a4612c34022e6761066e
Gabriela <gabrielita21.gb@gmail.com>
2021-06-09 11:15:57 +0200

    boton switch

--- resources/views/eventforms/create.blade.php
+++ resources/views/eventforms/create.blade.php
@@ -36,7 +36,12 @@
                             <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="description">
                         </div>
                         
-                        <input type="checkbox" class="btn-check sliderCB" id="btn-check-outlined" checked autocomplete="off">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                            <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+                        </div>
+
+                        <input type="checkbox" class="btn-check form-switch sliderCB" id="btn-check-outlined" checked autocomplete="off">
                         <label class="btn btn-outline-secondary" for="btn-check-outlined">carousel</label>
 
                         <div class="btnCreate">
