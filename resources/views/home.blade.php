9eebdb242358aef4f86df0c06619cc71ab580d38
Davidhdm <david.hdm9@gmail.com>
2021-06-14 14:14:38 +0200

    Componente recibe el array

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,82 +35,51 @@
     ?>
     <!-- Controls -->
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
         </button>
 </div>
 
 </main>
 
+<?php
+
+$show = null;
+
+?>
+
 <div>
-  <ul class="nav nav-tabs">
-    <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="#">Next events</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">My events</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Past events</a>
-    </li>
-  </ul>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" wire:click="$set('show', 1)" href="#">Next events</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" wire:click="$set('show', 2)" href="#">My events</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" wire:click="$set('show', 3)" href="#">Past events</a>
+        </li>
+    </ul>
 </div>
 
 <div class="container">
-    <section class="allevents">
+    
+    @if ($show == null || $show === 1)
+        <x-allevents :events="$events"/>
+    @endif
 
-        @foreach ($events as $event)
-            @if ($event->date_time > now())
-                <article class="eventContainer">
-                    <div class="eventInfo">
-                        <div class="dateAndUsers">
-                            <p>{{$event->date_time}}  </p>  
-                            <p>{{$event->users_max}} participantes</p>
-                        </div>
-                        
-                        <div class="titleAndDesc">
-                            <h3 class="eventTitle">{{$event->title}}</h3>
-                            <p class="eventDescription">{{$event->description}}</p>
-                        </div>
-                    </div>
-
-                    <div class="imgBtnContainer">
-                        <figure>
-                            <img class="imgEvents" src="{{$event->image}}" alt="">
-                        </figure>
-                        <button class="enrollBtn">Inscribirme</button>
-
-                        <td>
-                            <form action="{{ route('events.destroy',$event->id) }}" method="POST">
-                                <a class="btn btn-sm btn-primary" href="{{ route('show.show',$event->id) }}"><i class="fa fa-fw fa-eye"></i>🏷️</a>
-                                @if(Auth::check())
-                                    @if (Auth::user()->isAdmin)
-                                        <a class="btn btn-sm btn-success" href="{{ route('events.edit',$event->id) }}"><i class="fa fa-fw fa-edit"></i>✏️</a>
-                                    
-                                    @csrf
-                                    
-                                    @method('DELETE')
-                                    
-                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>❌</button>    
-                                    @endif
-                                @endif
-                                
-                            </form>
-                        </td>
-
-                    </div>
-
-                </article>
-                
-        <div class="line"></div>
-        @endif
-        @endforeach
-
-    </section>
+    @if ($show === 2)
+        <x-myevents/>
+    @endif
+    
+    @if ($show === 3)
+        <x-pastevents/>
+    @endif
+    
     <section class="asistiras"></section>
     <section class="pasados"></section>
 </div>
