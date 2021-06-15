933d17b1557214c6794fc391864e03b01f9549ed
Joan Recordà Pellicer <recorda@gmail.com>
2021-06-15 12:01:50 +0200

    actualización

--- resources/views/eventforms/show.blade.php
+++ resources/views/eventforms/show.blade.php
@@ -6,12 +6,13 @@
     <div class="card" >
         <img class="card-img-top w-100"  src="{{$event->image}}" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title cardTitle">{{$event->title}}</h5>
-            <p class="card-text cardDateText">{{$event->date_time}}</p>
+            <h5 class="card-title" id=
+            "cardTita">{{$event->title}}</h5>
+            <p class="card-text">{{$event->date_time}}</p>
         </div>
         <ul class="list-group list-group-flush">
             <li class="list-group-item cardDescriptionText">{{$event->description}}</li>
-            <li class="list-group-item card cardAttendees">{{$event->users_max}} participantes</li>
+            <li class="list-group-item cardAttendees"><p>{{$event->users_max}} participantes</li>
         </ul>
         <div class="card-body">
             <a href="#" class="card-link">Subscribe</a>
