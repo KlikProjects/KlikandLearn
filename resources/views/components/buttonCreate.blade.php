64b779270467cf20fe74a96f88ebe3919850d2da
Berta LI <you@example.com>
2021-06-16 12:24:31 +0200

    actualización styles buttonCreate

--- resources/views/components/buttonCreate.blade.php
+++ resources/views/components/buttonCreate.blade.php
@@ -1,2 +1,8 @@
-<div class="buttonCreate">
-    
+<div class="containerNewEvent">
+
+<button class="buttonCreate">
+    <a href="">
+        <img src="img/buttonCreate.png" class="imgButton" alt=""/> 
+    </a>
+    <p class="txtNewEvent">CREATE NEW EVENT</p>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,4 +73,14 @@
     </section>
 </div>
 
+<div class="containerNewEvent">
+
+<button class="buttonCreate">
+    <a href="">
+        <img src="img/buttonCreate.png" class="imgButton" alt=""/> 
+    </a>
+    <p class="txtNewEvent">CREATE NEW EVENT</p>
+</div>
+
+
 @endsection
