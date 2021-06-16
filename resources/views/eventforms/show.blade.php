7dc5b0372131eea9a487731daead6d994a287c1f
Joan Recordà Pellicer <recorda@gmail.com>
2021-06-16 09:04:54 +0200

    actualización

--- resources/views/eventforms/show.blade.php
+++ resources/views/eventforms/show.blade.php
@@ -6,8 +6,7 @@
     <div class="card" >
         <img class="card-img-top w-100"  src="{{$event->image}}" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title" id=
-            "cardTita">{{$event->title}}</h5>
+            <h5 class="card-title">{{$event->title}}</h5>
             <p class="card-text">{{$event->date_time}}</p>
         </div>
         <ul class="list-group list-group-flush">
