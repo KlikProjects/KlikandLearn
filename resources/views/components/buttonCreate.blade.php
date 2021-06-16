ffc2a89d39ec73ce0e3f62e79621ae6f1e4794cd
Gabriela <gabrielita21.gb@gmail.com>
2021-06-16 13:47:47 +0200

    estilos del boton create

--- resources/views/components/buttonCreate.blade.php
+++ resources/views/components/buttonCreate.blade.php
@@ -1,8 +1,8 @@
 <div class="containerNewEvent">
-
-<button class="buttonCreate">
-    <a href="">
-        <img src="img/buttonCreate.png" class="imgButton" alt=""/> 
-    </a>
+    <button class="buttonCreate">
+        <a href="">
+            <img src="img/buttonCreate.png" class="imgButton" alt=""/> 
+        </a>
+    </button>
     <p class="txtNewEvent">CREATE NEW EVENT</p>
 </div>
