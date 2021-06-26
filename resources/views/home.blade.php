67da0294564b4201f4b031015fb869b1111c8993
Gabriela <gabrielita21.gb@gmail.com>
2021-06-26 22:37:42 +0200

    responsive terminado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,23 +31,21 @@
             }
         ?>
     </div>
-
 </main>
 <div>
-
     <ul class="nav nav-pills mb-3 mt-3 eventsNav" id="pills-tab" role="tablist">
         <li class="nav-item" role="presentation">
-            <a class="nav-link allEv-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Next Events</a>
+            <a class="nav-link botonNav allEv-link active" id="pills-home-tab " data-bs-toggle="pill" data-bs-target="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Next Events</a>
         </li>
         
         <li class="nav-item myEv-link" role="presentation">
             @if (Auth::user())
-                <a class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">My Events</a>
+                <a class="nav-link botonNav" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">My Events</a>
             @endif
         </li>
         
         <li class="nav-item" role="presentation">
-            <a class="nav-link pastEv-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Past Events</a>
+            <a class="nav-link botonNav pastEv-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Past Events</a>
         </li>
     </ul>
 </nav>
@@ -64,5 +62,7 @@
         <x-pastevents :events="$events" :myeventuser="$myeventuser"/>
     </section>
 </div>
+
+<x-footer />
 
 @endsection
