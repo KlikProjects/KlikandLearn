fefd55cc2f73f8b0bcf34fa317bd04039e567be0
Berta LI <you@example.com>
2021-06-27 20:15:10 +0200

    actualización texto contact blade

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -5,17 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </head>
 <body>
 
-<h1>Welcome to Klik And Learn courses!</h1>
-
-<p>Congratulations, you have been subscribed to !</p>
-
-
-<p>Congratulations {{ $username }}, you have been subscribed to {{ $title }}! 
-
-Remember, this is gona be celebrated on the next {{ $date }} </p> 
+<div class="card">
+  <div class="card-header">
+  Welcome to Klik And Learn courses!
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">{{ $title }}</h5>
+    <p class="card-text">Congratulations {{ $username }}, you have been subscribed to {{ $title }}! 
+    We can't wait to meet you on the next {{ $date }} ! </p>
+    <a href="#" class="btn btn-primary">Go to the event!</a>
+  </div>
 
 </body>
 </html>
